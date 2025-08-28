1314df96c1b19c0a26a0d018bb586075e215d03e
yossep Binyoum <yossep237@gmail.com>
2025-08-28 10:53:50 +0000

    update main

--- main.py
+++ main.py
@@ -3,18 +3,16 @@ from flask import (Flask, render_template, redirect, request, url_for)
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+comments = []
 
 @app.route("/", methods=["POST", "GET"])
 def index():
     """ Home page"""
-
-    comments = []
-    if request.method == "POST":
-        
-        comment = request.form['contents']
-        print("Comment: ", comment)
-        comments.append(comment)
-        return redirect(url_for('index'))
     
-    return render_template("index.html", comments=comments)
+    if request.method == "GET":
+        return render_template("index.html", comments=comments)
+    
+    comments.append(request.form["contents"])
+    return redirect(url_for('index'))
+    
 
