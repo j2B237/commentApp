06c0fdd34b32eebf5ed221b125415555a46b47fb
yossep Binyoum <yossep237@gmail.com>
2025-08-28 12:51:03 +0000

    add login route and func

--- main.py
+++ main.py
@@ -40,5 +40,8 @@ def index():
         db.session.commit()
     
     return redirect(url_for('index'))
-    
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    return render_template("login.html")
 
