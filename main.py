592a14482c0e65893ac12477e717fd47b948ec58
yossep Binyoum <yossep237@gmail.com>
2025-08-29 20:09:12 +0000

    update comment table

--- main.py
+++ main.py
@@ -67,6 +67,7 @@ class Comment(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(4096))
+    posted = db.Column(db.DateTime, default=datetime.now())
 
 
 @login_manager.user_loader
